51d10a51c6609899f30105b56b2b772180931a22
Brian Shaginaw <bshaginaw120@gmail.com>
2026-07-05 14:56:04 -0400

    Make cell suggestions scrollable
    This makes narrower windows a bit nicer.
    
    Now that we have scroll I'd also like to include more than 100 results,
    but unforch this extra rendering causes unacceptable slowdown. Will need
    to look into delayed rendering, or something.

--- src/CellInfo.svelte
+++ src/CellInfo.svelte
@@ -20,82 +20,113 @@
 </script>
 
 <div id="cell-data">
-  <ul>
-    <li>{acrossFills?.length ?? "?"} potential across fills.
-      {#if acrossFills}
-        <div class="word-grid">
+  <div class="section">
+    {acrossFills?.length ?? "?"} potential across fills.
+    {#if acrossFills}
+      <div class="section-contents">
+        <ol class="word-grid">
           {#each acrossFills.slice(0, suggestedWordLimit) as entry}
-            <a on:click={() => gridCallbacks.fillAcross({
-                word: entry.entry.word,
-                pivotIdx: entry.pivotIdx,
-              })}
-              on:mouseover={() => gridCallbacks.previewAcross({
-                word: entry.entry.word,
-                pivotIdx: entry.pivotIdx,
-              })}
-              on:mouseout={() => gridCallbacks.clearPreview()}
-            >
-              {@html highlightEntry(entry)}
-            </a>
+            <li>
+              <a on:click={() => gridCallbacks.fillAcross({
+                  word: entry.entry.word,
+                  pivotIdx: entry.pivotIdx,
+                })}
+                on:mouseover={() => gridCallbacks.previewAcross({
+                  word: entry.entry.word,
+                  pivotIdx: entry.pivotIdx,
+                })}
+                on:mouseout={() => gridCallbacks.clearPreview()}
+              >
+                {@html highlightEntry(entry)}
+              </a>
+            </li>
           {/each}
-        </div>
-      {/if}
-    </li>
-    <li>{downFills?.length ?? "?"} potential down fills.
-      {#if downFills}
-        <div class="word-grid">
+        </ol>
+      </div>
+    {/if}
+  </div>
+  <div class="section">
+    {downFills?.length ?? "?"} potential down fills.
+    {#if downFills}
+      <div class="section-contents">
+        <ol class="word-grid">
           {#each downFills.slice(0, suggestedWordLimit) as entry}
-            <a on:click={() => gridCallbacks.fillDown({
-                word: entry.entry.word,
-                pivotIdx: entry.pivotIdx,
-              })}
-              on:mouseover={() => gridCallbacks.previewDown({
-                word: entry.entry.word,
-                pivotIdx: entry.pivotIdx,
-              })}
-              on:mouseout={() => gridCallbacks.clearPreview()}
-            >
-              {@html highlightEntry(entry)}
-            </a>
+            <li>
+              <a on:click={() => gridCallbacks.fillDown({
+                  word: entry.entry.word,
+                  pivotIdx: entry.pivotIdx,
+                })}
+                on:mouseover={() => gridCallbacks.previewDown({
+                  word: entry.entry.word,
+                  pivotIdx: entry.pivotIdx,
+                })}
+                on:mouseout={() => gridCallbacks.clearPreview()}
+              >
+                {@html highlightEntry(entry)}
+              </a>
+            </li>
           {/each}
-        </div>
-      {/if}
-    </li>
-    <li>{cellFills?.length ?? "?"} potential fills for this cell:
-      {#if cellFills}
-        <div class="letter-grid">
+        </ol>
+      </div>
+    {/if}
+  </div>
+  <div class="section">
+    {cellFills?.length ?? "?"} potential fills for this cell:
+    {#if cellFills}
+      <div class="section-contents">
+        <ol class="letter-grid">
           {#each cellFills as fill}
-            <a
+            <li><a
               on:click={() => gridCallbacks.fillCell({ fill })}
               on:mouseover={() => gridCallbacks.previewCell({ fill })}
               on:mouseout={() => gridCallbacks.clearPreview()}
-            >{fill}</a>
+            >{fill}</a></li>
           {/each}
-        </div>
-      {/if}
-    </li>
-  </ul>
+        </ol>
+      </div>
+    {/if}
+  </div>
 </div>
 
 <style>
   #cell-data {
-    overflow: auto;
     background-color: lightyellow;
     font-family: Arial;
+    padding: 10px 0;
   }
 
-  .word-grid {
+  .section {
+    padding: 0 15px;
+  }
+
+  .section-contents {
+    max-height: 20.5em;
+    overflow-y: scroll;
+    background-color: #ffe;
+    border: solid 1px #cc9;
+    margin-top: 3px;
+    margin-bottom: 10px;
+  }
+
+  ol {
+    list-style-type: none;
+    padding-left: 15px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
+
+  ol.word-grid {
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   }
 
-  .word-grid > * {
+  ol.word-grid > li {
     overflow: hidden;
     text-overflow: ellipsis;
     margin-right: 10px;
   }
 
-  .letter-grid {
+  ol.letter-grid {
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(2em, 1fr));
   }
